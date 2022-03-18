45279ee211a5fb6110e6e375ed50d818dd02d6fc
Erika Hunhoff <hunhoffe@vmware.com>
2022-03-18 13:04:24 -0600

    Add initial schema

--- src/main/java/com/vmware/bespin/scheduler/Runner.java
+++
@@ -1,7 +0,0 @@
-package com.vmware.bespin.scheduler;
-
-public class Runner {
-    public static void main(String args[]) {
-        System.out.println("Test");
-    }
-}
---
+++ src/main/resources/bespin_tables.sql
@@ -0,0 +1,21 @@
+create table nodes(id integer,
+    cores integer,
+    memory_slices integer);
+
+create table applications(id integer);
+
+create table app_cores(id integer,
+    application integer,
+	current_node integer,
+	controllable__node integer,
+	foreign key (controllable__node) references nodes(id),
+	foreign key (application) references applications(id),
+	primary key (application, id));
+
+create table mem_slices(id integer,
+    application integer,
+    current_node integer,
+    controllable__node integer,
+    foreign key (controllable__node) references nodes(id),
+    foreign key (application) references applications(id),
+    primary key (application, id));
