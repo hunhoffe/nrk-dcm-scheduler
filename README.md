f0167fae26b60b8afa9fc3a96c997f0a39eeeb11
Erika Hunhoff <hunhoffe@vmware.com>
2022-03-18 14:07:50 -0600

    Use older version of jooq to fix errors, update readme

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # DCM Controller Scheduler
 
 ## Build
+
+Generate Java helper classes by on the bespin_schema.sql file:
+```bash
+mvn jooq-codegen:generate
+```
+
 Build with:
 ```bash
 mvn install
--- src/main/resources/bespin_tables.sql
+++ src/main/resources/bespin_tables.sql
@@ -1,21 +1,30 @@
-create table nodes(id integer,
+create table nodes(
+    id integer,
     cores integer,
-    memory_slices integer);
+    memory_slices integer
+);
 
-create table applications(id integer);
+create table applications(
+    id integer,
+    
+);
 
-create table app_cores(id integer,
+create table app_cores(
+    id integer,
     application integer,
 	current_node integer,
 	controllable__node integer,
 	foreign key (controllable__node) references nodes(id),
 	foreign key (application) references applications(id),
-	primary key (application, id));
+	primary key (application, id)
+);
 
-create table mem_slices(id integer,
+create table mem_slices(
+    id integer,
     application integer,
     current_node integer,
     controllable__node integer,
     foreign key (controllable__node) references nodes(id),
     foreign key (application) references applications(id),
-    primary key (application, id));
+    primary key (application, id)
+);
