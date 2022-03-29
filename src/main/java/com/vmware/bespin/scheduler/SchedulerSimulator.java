0c577e7f24c8ad25584b40500ecab6fee5d08aaf
Erika Hunhoff <hunhoffe@vmware.com>
2022-03-29 13:23:58 -0600

    Now can access database

--- src/main/java/com/vmware/bespin/scheduler/SchedulerSimulator.java
+++ src/main/java/com/vmware/bespin/scheduler/SchedulerSimulator.java
@@ -1,8 +1,22 @@
 package com.vmware.bespin.scheduler;
 
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+import org.jooq.Result;
+import org.jooq.Record;
+import org.jooq.Update;
+
 public class SchedulerSimulator {
 
-    public static void main(String[] argv) {
+    public static void main(String[] argv) throws ClassNotFoundException {
+        // Create an in-memory database and get a JOOQ connection to it
+        Class.forName ("org.h2.Driver");
+        final DSLContext conn = DSL.using("jdbc:h2:mem:");
+
+        // node == physical machine / server
+        conn.execute("create table nodes(" +
+                "id integer)");
+
         System.out.println("this is the main method!");
     }
 }
