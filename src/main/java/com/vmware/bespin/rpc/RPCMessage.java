9019ff82a83be48c70f357d1ea915599a137a2f8
Erika Hunhoff <hunhoffe@vmware.com>
2022-07-08 07:13:05 -0700

    Added MIT license too and source code headers

--- src/main/java/com/vmware/bespin/rpc/RPCMessage.java
+++ src/main/java/com/vmware/bespin/rpc/RPCMessage.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2022 VMware, Inc. All Rights Reserved.
+ * SPDX-License-Identifier: BSD-2 OR MIT
+ */
+
 package com.vmware.bespin.rpc;
 
 public record RPCMessage(RPCHeader hdr, byte[] payload) { }
