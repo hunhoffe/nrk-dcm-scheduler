c4d32891abdf791f3e8b93f9ea586ce65422adfd
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2022-09-02 09:47:28 -0600

    Update and rename maven_install.sh to dev_install.sh

--- dev_install.sh
+++ dev_install.sh
@@ -1,10 +1,16 @@
+# Install Java
+sudo apt update
+sudo apt install -y openjdk-16-jre openjdk-16-jdk
+
+# Setup for maven install
 mvn_version=${mvn_version:-3.8.6}
 url="http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/${mvn_version}/binaries/apache-maven-${mvn_version}-bin.tar.gz"
 install_dir="/opt/maven"
 
-mkdir ${install_dir}
-curl -fsSL ${url} | tar zx --strip-components=1 -C ${install_dir}
-cat << EOF > /etc/profile.d/maven.sh
+# Install maven
+sudo mkdir ${install_dir}
+sudo curl -fsSL ${url} | sudo tar zx --strip-components=1 -C ${install_dir}
+sudo tee /etc/profile.d/maven.sh > /dev/null << EOF
 #!/bin/sh
 export MAVEN_HOME=${install_dir}
 export M2_HOME=${install_dir}
@@ -15,3 +21,6 @@ source /etc/profile.d/maven.sh
 echo maven installed to ${install_dir}
 mvn --version
 echo "source /etc/profile.d/maven.sh" >>  ~/.bashrc
+
+# Install minizinc
+sudo apt install minizinc
