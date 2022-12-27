c0b64959641a7d4b1b0ce36a602669f75824ca5f
Erika Hunhoff <54562339+hunhoffe@users.noreply.github.com>
2022-12-27 15:25:15 -0800

    Simplify RPC header and update protobufs (#12)
    * Updated protobuf to address github dependabot alerts
    * simplify header

--- src/main/java/com/vmware/bespin/rpc/RPCHeader.java
+++ src/main/java/com/vmware/bespin/rpc/RPCHeader.java
@@ -6,38 +6,26 @@
 package com.vmware.bespin.rpc;
 
 public class RPCHeader {
-        public static final int BYTE_LEN = Long.BYTES * 5;
+        public static final int BYTE_LEN = Long.BYTES * 2;
 
-        public final long clientId;    // 8 bytes
-        public final long pid;         // 8 bytes
-        public final long reqId;       // 8 bytes
         private final long msgType;     // 8 byte
-        public long msgLen;            // 8 bytes
+        public long msgLen;             // 8 bytes
 
-        public RPCHeader(final long clientId, final long pid, final long reqId, final long msgType, final long msgLen) {
-                this.clientId = clientId;
-                this.pid = pid;
-                this.reqId = reqId;
+        public RPCHeader(final long msgType, final long msgLen) {
                 this.msgType = msgType;
                 this.msgLen = msgLen;
         }
 
         public RPCHeader(final byte[] data) {
                 assert (data.length == RPCHeader.BYTE_LEN);
-                this.clientId = Utils.bytesToLong(data, 0);
-                this.pid = Utils.bytesToLong(data, Long.BYTES);
-                this.reqId = Utils.bytesToLong(data, Long.BYTES * 2);
-                this.msgType = Utils.bytesToLong(data, Long.BYTES * 3);
-                this.msgLen = Utils.bytesToLong(data, Long.BYTES * 4);
+                this.msgType = Utils.bytesToLong(data, 0);
+                this.msgLen = Utils.bytesToLong(data, Long.BYTES);
         }
 
         public byte[] toBytes() {
                 final byte[] buff = new byte[RPCHeader.BYTE_LEN];
-                Utils.longToBytes(this.clientId, buff, 0);
-                Utils.longToBytes(this.pid, buff, Long.BYTES);
-                Utils.longToBytes(this.reqId, buff, Long.BYTES * 2);
-                Utils.longToBytes(this.msgType, buff, Long.BYTES * 3);
-                Utils.longToBytes(this.msgLen, buff, Long.BYTES * 4);
+                Utils.longToBytes(this.msgType, buff, 0);
+                Utils.longToBytes(this.msgLen, buff, Long.BYTES);
                 return buff;
         }
 
@@ -47,7 +35,6 @@ public class RPCHeader {
 
         @Override
         public String toString() {
-                return String.format("RPCHdr(id=%d, pid=%d, reqId=%d, type=%d, len=%d)", 
-                                        this.clientId, this.pid, this.reqId, this.msgType, this.msgLen);
+                return String.format("RPCHdr(type=%d, len=%d)", this.msgType, this.msgLen);
         }
 }
