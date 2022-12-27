547b11d8b50c47147ccaf44988b6506fc5cb714f
Erika Hunhoff <hunhoff.erika@gmail.com>
2022-12-27 15:15:21 -0800

    simplify header

--- src/test/java/com/vmware/bespin/rpc/TestRPCHeader.java
+++ src/test/java/com/vmware/bespin/rpc/TestRPCHeader.java
@@ -11,13 +11,10 @@ public class TestRPCHeader {
 
     @Test
     public void testConversion() {
-        RPCHeader hdr = new RPCHeader(1L, 2L, 3L, (byte) 4, 5L);
+        RPCHeader hdr = new RPCHeader((byte) 4, 5L);
         byte[] b = hdr.toBytes();
         assert(b.length == RPCHeader.BYTE_LEN);
         RPCHeader hdr2 = new RPCHeader(b);
-        assert(hdr2.clientId == hdr.clientId);
-        assert(hdr2.pid == hdr.pid);
-        assert(hdr2.reqId == hdr.reqId);
         assert(hdr2.getType() == hdr.getType());
         assert(hdr2.msgLen == hdr.msgLen);
     }
