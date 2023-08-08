984c8199cb9c40ebbb057dd92fdab8605a0821c2
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-08-08 08:15:59 -0700

    Running first benchmarks with DCM-LOC

---
+++ scripts/README.md
@@ -0,0 +1,9 @@
+# Overview of Benchmarking Scripts
+
+Before running any of the scripts, generate the test binary by running ```mvn clean package``` from the root of the repo.
+
+```old_microbenchmark.sh``` is the original benchmarking script of Mar 2022.
+It may not work any  more.
+
+
+```latency_scale_microbenchmark.sh``` is the benchmarking script for looking at the latency of DCM as it scale.
---
+++ scripts/latency_scale_microbenchmark.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+JAR_PATH="../target/scheduler-1.1.16-SNAPSHOT-jar-with-dependencies.jar"
+MAIN_CLASS="com.vmware.bespin.scheduler.DCMSimulation"
+
+ITERS_PER_TEST=20
+BATCH_SIZES=(1)
+
+# The lengths of all the config arrays need to be the same
+MACHINE_CONFIGS=(4 8 16 32)
+CORE_CONFIGS=(16 32 64 128)
+MEMSLICE_CONFIGS=(128 256 512 1024)
+# The number of different rack test configurations to run
+NUM_CONFIGS=${#MACHINE_CONFIGS[@]}
+
+# % of cluster to fill
+RACK_UTILS=(10 50 90)
+RACK_FILL="poisson"
+
+# Create output dir or bail
+OUTPUT_DIR="results"
+mkdir $OUTPUT_DIR || exit -1
+
+# Iterate over rack configs
+for (( config_idx=0; config_idx<${NUM_CONFIGS}; config_idx++ ));
+do
+  num_machines=${MACHINE_CONFIGS[$config_idx]}
+  num_cores=${CORE_CONFIGS[$config_idx]}
+  num_memslices=${MEMSLICE_CONFIGS[$config_idx]}
+  num_processes=$(( 4*num_machines ))
+
+  # Iterate over rack utilization
+  for rack_util in "${RACK_UTILS[@]}"
+  do
+    # Iterate over different batch sizes
+    for batch_size in "${BATCH_SIZES[@]}"
+    do
+      # Perform a specific numbers of iterations for this particular test config
+      for (( iter=0; iter<$ITERS_PER_TEST; iter++ ))
+      do
+        output_file="$RACK_FILL-$batch_size-$num_machines-$num_cores-$num_memslices-$num_processes-$rack_util-$iter.log"
+        cmd="java -cp $JAR_PATH $MAIN_CLASS -a $batch_size -c $num_cores -f $RACK_FILL -m $num_memslices -n $num_machines -p $num_processes -u $rack_util"
+        echo "Fill: $RACK_FILL, Batch: $batch_size, ClusterConfig: (${num_machines} ${num_cores} ${num_memslices}) Util: $rack_util Iter: $iter OutputFile: $output_file"
+        echo $cmd
+        echo $cmd > $OUTPUT_DIR/$output_file
+        $cmd >> $OUTPUT_DIR/$output_file 2>&1
+      done
+    done
+  done
+done
