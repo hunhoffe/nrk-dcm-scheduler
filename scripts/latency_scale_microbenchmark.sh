84f812a821b2e242f4ba78380c32d4a9f05b572f
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-08-08 08:40:07 -0700

    update microbenchmark script

--- scripts/latency_scale_microbenchmark.sh
+++ scripts/latency_scale_microbenchmark.sh
@@ -17,33 +17,40 @@ NUM_CONFIGS=${#MACHINE_CONFIGS[@]}
 RACK_UTILS=(10 50 90)
 RACK_FILL="poisson"
 
+SCHEDULERS="R RR DCMcap"
+# SCHEDULERS="R RR DCMcap DCMloc"
+
 # Create output dir or bail
 OUTPUT_DIR="results"
 mkdir $OUTPUT_DIR || exit -1
 
-# Iterate over rack configs
-for (( config_idx=0; config_idx<${NUM_CONFIGS}; config_idx++ ));
+# Iterate over schedulers
+for sched in $SCHEDULERS
 do
-  num_machines=${MACHINE_CONFIGS[$config_idx]}
-  num_cores=${CORE_CONFIGS[$config_idx]}
-  num_memslices=${MEMSLICE_CONFIGS[$config_idx]}
-  num_processes=$(( 4*num_machines ))
-
-  # Iterate over rack utilization
-  for rack_util in "${RACK_UTILS[@]}"
+  # Iterate over rack configs
+  for (( config_idx=0; config_idx<${NUM_CONFIGS}; config_idx++ ));
   do
-    # Iterate over different batch sizes
-    for batch_size in "${BATCH_SIZES[@]}"
+    num_machines=${MACHINE_CONFIGS[$config_idx]}
+    num_cores=${CORE_CONFIGS[$config_idx]}
+    num_memslices=${MEMSLICE_CONFIGS[$config_idx]}
+    num_processes=$(( 4*num_machines ))
+
+    # Iterate over rack utilization
+    for rack_util in "${RACK_UTILS[@]}"
     do
-      # Perform a specific numbers of iterations for this particular test config
-      for (( iter=0; iter<$ITERS_PER_TEST; iter++ ))
+      # Iterate over different batch sizes
+      for batch_size in "${BATCH_SIZES[@]}"
       do
-        output_file="$RACK_FILL-$batch_size-$num_machines-$num_cores-$num_memslices-$num_processes-$rack_util-$iter.log"
-        cmd="java -cp $JAR_PATH $MAIN_CLASS -a $batch_size -c $num_cores -f $RACK_FILL -m $num_memslices -n $num_machines -p $num_processes -u $rack_util"
-        echo "Fill: $RACK_FILL, Batch: $batch_size, ClusterConfig: (${num_machines} ${num_cores} ${num_memslices}) Util: $rack_util Iter: $iter OutputFile: $output_file"
-        echo $cmd
-        echo $cmd > $OUTPUT_DIR/$output_file
-        $cmd >> $OUTPUT_DIR/$output_file 2>&1
+        # Perform a specific numbers of iterations for this particular test config
+        for (( iter=0; iter<$ITERS_PER_TEST; iter++ ))
+        do
+          output_file="$sched-$RACK_FILL-$batch_size-$num_machines-$num_cores-$num_memslices-$num_processes-$rack_util-$iter.log"
+          cmd="java -cp $JAR_PATH $MAIN_CLASS -a $batch_size -c $num_cores -f $RACK_FILL -m $num_memslices -n $num_machines -p $num_processes -u $rack_util -s $sched"
+          echo "Fill: $RACK_FILL, Batch: $batch_size, Scheduler: $sched, ClusterConfig: (${num_machines} ${num_cores} ${num_memslices}) Util: $rack_util Iter: $iter OutputFile: $output_file"
+          echo $cmd
+          echo $cmd > $OUTPUT_DIR/$output_file
+          $cmd >> $OUTPUT_DIR/$output_file 2>&1
+        done
       done
     done
   done
