8d0f7caa1c99cbaee814e838a990609241758ada
Erika Hunhoff <hunhoff.erika@gmail.com>
2024-06-22 20:43:28 -0700

    Update install information

--- dev_install.sh
+++ dev_install.sh
@@ -1,14 +1,15 @@
 # Install Java
 sudo apt update
-sudo apt install -y openjdk-16-jre openjdk-16-jdk
+sudo apt install -y openjdk-17-jre openjdk-17-jdk
 
 # Setup for maven install
-mvn_version=${mvn_version:-3.9.0}
-url="http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/${mvn_version}/binaries/apache-maven-${mvn_version}-bin.tar.gz"
+mvn_version=${mvn_version:-3.9.8}
+#url="http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/${mvn_version}/binaries/apache-maven-${mvn_version}-bin.tar.gz"
+url="https://dlcdn.apache.org/maven/maven-3/${mvn_version}/binaries/apache-maven-${mvn_version}-bin.tar.gz"
 install_dir="/opt/maven"
 
 # Install maven
-sudo mkdir ${install_dir}
+sudo mkdir -p ${install_dir}
 sudo curl -fsSL ${url} | sudo tar zx --strip-components=1 -C ${install_dir}
 sudo tee /etc/profile.d/maven.sh > /dev/null << EOF
 #!/bin/sh
